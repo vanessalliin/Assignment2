52c42721282803044ffc5c70e3ab94a5bdbbde9c
vanessalliin <123032124+vanessalliin@users.noreply.github.com>
2025-03-21 15:49:46 +0000

    fix

--- wp-content/themes/chatbot/functions.php
+++ wp-content/themes/chatbot/functions.php
@@ -12,7 +12,7 @@ function theme_enqueue_styles() {
     );
     wp_enqueue_script('custom_javascript', get_stylesheet_directory_uri() . '/customElements.js');
     wp_enqueue_script_module('custom_module1', "https://vanessalliin.github.io/Assignment1/index.js");
-    wp_enqueue_script_module('custom_module2', "https://rhildred.github.io/chatter/fab.js");
+    wp_enqueue_script_module('custom_module2', "https://vanessalliin.github.io/Assignment1/fab.js");
 }
 //
 // Your code goes below
